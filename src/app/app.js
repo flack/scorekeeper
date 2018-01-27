1e193977b6871621df1a0091d53bcbd360ee57e4
flack <flack@contentcontrol-berlin.de>
2018-01-27 21:15:08 +0100

    Replace outdated hook

--- src/app/app.js
+++ src/app/app.js
@@ -10,18 +10,14 @@ angular.module( 'ngScorekeeper', [
 ])
 
 .config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
-  $urlRouterProvider.otherwise( '/home' );
+    $urlRouterProvider.otherwise( '/home' );
 })
 
 .run( function run () {
 })
 
-.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
-  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
-    if ( angular.isDefined( toState.data.pageTitle ) ) {
-      $scope.pageTitle = toState.data.pageTitle + ' | scorekeeper' ;
-    }
-  });
-})
-
-;
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location, $transitions ) {
+      $transitions.onSuccess({}, function($transition) {
+          $scope.pageTitle = $transition.to().data.pageTitle + ' | scorekeeper';
+      });
+});
--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -1,13 +1,14 @@
 describe( 'AppCtrl', function() {
   describe( 'isCurrentUrl', function() {
-    var AppCtrl, $location, $scope;
+    var AppCtrl, $location, $scope, $transitions;
 
     beforeEach( module( 'ngScorekeeper' ) );
 
-    beforeEach( inject( function( $controller, _$location_, $rootScope ) {
+    beforeEach( inject( function( $controller, _$location_, $rootScope, _$transitions_ ) {
       $location = _$location_;
+      $transitions = _$transitions_;
       $scope = $rootScope.$new();
-      AppCtrl = $controller( 'AppCtrl', { $location: $location, $scope: $scope });
+      AppCtrl = $controller( 'AppCtrl', { $location: $location, $scope: $scope, $transitions: $transitions });
     }));
 
     it( 'should pass a dummy test', inject( function() {
